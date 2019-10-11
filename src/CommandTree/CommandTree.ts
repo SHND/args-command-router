2f7037fbe444b4bc8602be98b73d4a21e74cf2c6
Sahand Javid <sahand.javid@gmail.com>
2019-10-10 23:18:34 -0700

    CommandTree

--- src/CommandTree/CommandTree.ts
+++ src/CommandTree/CommandTree.ts
@@ -2,7 +2,7 @@ import CommandNode from './CommandNode'
 import FixedCommandNode from './FixedCommandNode'
 
 export default class CommandTree {
-  private _rootNode: CommandNode = new FixedCommandNode('')
+  private _rootNode: CommandNode = new FixedCommandNode('/')
 
   constructor(rootNode?: CommandNode) {
     if (rootNode !== undefined) this._rootNode = rootNode
