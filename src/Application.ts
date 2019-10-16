ef6dcf1139892c9bee42fc7acf62bd484cff855e
Sahand Javid <sahand.javid@gmail.com>
2019-10-16 01:46:49 -0700

    externalize Application::run logic

--- src/Application.ts
+++ src/Application.ts
@@ -96,54 +96,107 @@ export default class Application {
     this._norouteCallback = callback
   }
 
-  run(argv?: string[]) {
-    const acpData: Data = parser(argv).data
-    const { commands: commandItems, shortSwitches, longSwitches } = acpData
+  private cleanSwitchValues(acpSwitches: {
+    [key: string]: string[]
+  }): { [key: string]: string | boolean } {
+    const output: { [key: string]: string | boolean } = {}
 
-    const cleanedShortSwitches: { [key: string]: string | boolean } = {}
-    for (let key in shortSwitches)
-      cleanedShortSwitches[key] =
-        shortSwitches[key].length > 0 ? shortSwitches[key][0] : true
+    for (let key in acpSwitches)
+      output[key] = acpSwitches[key].length > 0 ? acpSwitches[key][0] : true
 
-    const cleanedLongSwitches: { [key: string]: string | boolean } = {}
-    for (let key in longSwitches)
-      cleanedLongSwitches[key] =
-        longSwitches[key].length > 0 ? longSwitches[key][0] : true
+    return output
+  }
 
-    const commandParamValues = []
-
+  private findCommandNode(commandValues: string[]): CommandNode | null {
     let currentNode: CommandNode | null = this._commandTree.root
 
-    for (let commandItem of commandItems) {
-      if (currentNode && currentNode.children[commandItem]) {
-        currentNode = currentNode.children[commandItem]
+    for (let commandValue of commandValues) {
+      if (currentNode && currentNode.children[commandValue]) {
+        currentNode = currentNode.children[commandValue]
       } else if (currentNode && currentNode.parameterChild) {
         currentNode = currentNode.parameterChild
-        commandParamValues.push(commandItem)
       } else {
-        const callbackData: CallbackData = new CallbackData(
-          null,
-          {},
-          cleanedShortSwitches,
-          cleanedLongSwitches
-        )
-        this._norouteCallback(callbackData)
-        return
+        return null
       }
     }
 
-    const command: Command | null = currentNode.getCommand()
-    if (!command) return null
+    return currentNode
+  }
 
-    const commandParamNames = command.getParameters()
+  private getCommandParamsArray(commandValues: string[]): string[] {
+    const commandParamValues = []
+    let currentNode: CommandNode | null = this._commandTree.root
+
+    for (let commandValue of commandValues) {
+      if (currentNode && currentNode.children[commandValue]) {
+        currentNode = currentNode.children[commandValue]
+      } else if (currentNode && currentNode.parameterChild) {
+        currentNode = currentNode.parameterChild
+        commandParamValues.push(commandValue)
+      } else {
+        return commandParamValues
+      }
+    }
+
+    return commandParamValues
+  }
+
+  private mapCommandParamNamesAndValues(
+    commandParamNames: string[],
+    commandParamValues: string[]
+  ): { [key: string]: string } {
+    const parameters: { [key: string]: string } = {}
+
     if (commandParamValues.length !== commandParamNames.length)
       throw Error(
         'Expecting to have exact same number of command parameters and values'
       )
 
-    const parameters: { [key: string]: string } = {}
     for (let i = 0; i < commandParamValues.length; i++)
       parameters[commandParamNames[i]] = commandParamValues[i]
+
+    return parameters
+  }
+
+  run(argv?: string[]) {
+    const acpData: Data = parser(argv).data
+    const { commands: commandItems, shortSwitches, longSwitches } = acpData
+
+    const cleanedShortSwitches: {
+      [key: string]: string | boolean
+    } = this.cleanSwitchValues(shortSwitches)
+
+    const cleanedLongSwitches: {
+      [key: string]: string | boolean
+    } = this.cleanSwitchValues(longSwitches)
+
+    let currentNode: CommandNode | null = this.findCommandNode(commandItems)
+
+    if (!currentNode) {
+      const callbackData = new CallbackData(
+        null,
+        {},
+        cleanedShortSwitches,
+        cleanedLongSwitches
+      )
+      this._norouteCallback(callbackData)
+      return
+    }
+
+    const commandParamValues = this.getCommandParamsArray(commandItems)
+
+    const command: Command | null = currentNode.getCommand()
+
+    if (!command) return null
+
+    const commandParamNames = command.getParameters()
+
+    const parameters: {
+      [key: string]: string
+    } = this.mapCommandParamNamesAndValues(
+      commandParamNames,
+      commandParamValues
+    )
 
     for (let key in shortSwitches) {
       if (parameters[key])
