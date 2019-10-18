bbb177dff6a2bbea557589b3157a54f4d312a601
Sahand Javid <sahand.javid@gmail.com>
2019-10-17 18:22:33 -0700

    Add undefined support for conditions

--- src/Condition.ts
+++ src/Condition.ts
@@ -1,14 +1,39 @@
 import { Parser, Expression } from 'expr-eval'
 
+const exprEvalOptions = {
+  operators: {
+    // These default to true, but are included to be explicit
+    add: false,
+    concatenate: false,
+    conditional: false,
+    divide: false,
+    factorial: false,
+    multiply: false,
+    power: false,
+    remainder: false,
+    subtract: false,
+
+    // Disable and, or, not, <, ==, !=, etc.
+    logical: true,
+    comparison: true,
+
+    // Disable 'in' and = operators
+    in: false,
+    assignment: false,
+  },
+}
+
 export default class Condition {
+  private _parser: Parser
   private _expressionStr: string
   private _expression: Expression | null
 
   constructor(expression?: string) {
+    this._parser = new Parser(exprEvalOptions)
     if (expression) {
       this._expressionStr = expression
       try {
-        this._expression = Parser.parse(expression)
+        this._expression = this._parser.parse(expression)
       } catch {
         throw Error(`Failed to parse expression '${expression}'.`)
       }
@@ -24,15 +49,16 @@ export default class Condition {
   }
 
   evaluate(values: { [key: string]: string | boolean }): boolean {
-    const newValues: { [key: string]: string } = {}
+    if (!this._expression) return true
 
-    if (this._expression) {
-      for (let key of this._expression.variables())
-        newValues[key] = String(values[key])
+    // Creating function out of expression to be abale to evaluate expression with undefined variables
+    const inputs = this._expression.variables().sort()
+    const inputsStr = inputs.join(',')
+    const evaluate = this._expression.toJSFunction(inputsStr)
 
-      return this._expression.evaluate(newValues)
-    }
+    const newValueList = []
+    for (let key of inputs) newValueList.push(values[key])
 
-    return true
+    return Boolean(evaluate(...newValueList))
   }
 }
