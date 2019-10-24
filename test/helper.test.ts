9051d294f58668de9543e0007f336e8d592b862e
Sahand Javid <sahand.javid@gmail.com>
2019-10-24 01:18:14 -0700

    Unit test: Application, helper

---
+++ test/helper.test.ts
@@ -0,0 +1,459 @@
+import { expect } from 'chai'
+import Command from '../src/Command'
+import {
+  validateOneValueMostForEachSwitch,
+  validateNonConflictSwitches,
+  validateNonConflictParamsAndSwitches,
+  validateAllRequiredSwitchesPresent,
+  validateInputRequiredSwitchesHaveValue,
+  validateInputValuedSwitchesWithNoDefaultHaveValue,
+  validateInputBooleanSwitchesDontHaveValues,
+  defaultShortValuedSwitchesThatNeedsToBeAdded,
+  defaultLongValuedSwitchesThatNeedsToBeAdded,
+  cleanSwitchValues,
+  mapCommandParamNamesAndValues,
+} from '../src/helpers'
+
+describe('helper methods', () => {
+  it('validateOneValueMostForEachSwitch() if no more than one value exist', () => {
+    const acpSwitches = {
+      a: [],
+      b: ['cmd1'],
+      c: ['cmd2'],
+    }
+
+    expect(() => {
+      validateOneValueMostForEachSwitch(acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateOneValueMostForEachSwitch() when more than one value exist', () => {
+    const acpSwitches = {
+      a: [],
+      b: ['cmd1'],
+      c: ['cmd2', 'cmd3'],
+    }
+
+    expect(() => {
+      validateOneValueMostForEachSwitch(acpSwitches)
+    }).throws()
+  })
+
+  it('validateNonConflictSwitches() when no conflict exist', () => {
+    const acpSwitches1 = {
+      a: [],
+      b: ['cmd1'],
+      c: ['cmd2', 'cmd3'],
+    }
+
+    const acpSwitches2 = {
+      d: [],
+      e: ['cmd1'],
+      f: ['cmd2', 'cmd3'],
+    }
+
+    expect(() => {
+      validateNonConflictSwitches(acpSwitches1, acpSwitches2)
+    }).not.throws()
+  })
+
+  it('validateNonConflictSwitches() when conflict exist', () => {
+    const acpSwitches1 = {
+      a: [],
+      b: ['cmd1'],
+      c: ['cmd2', 'cmd3'],
+    }
+
+    const acpSwitches2 = {
+      d: [],
+      e: ['cmd1'],
+      b: ['cmd2', 'cmd3'],
+    }
+
+    expect(() => {
+      validateNonConflictSwitches(acpSwitches1, acpSwitches2)
+    }).throws()
+  })
+
+  it('validateNonConflictParamsAndSwitches() when no conflict exist', () => {
+    const params1 = {
+      a: '',
+      b: 'a',
+      c: 'b',
+    }
+
+    const acpSwitches2 = {
+      d: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateNonConflictParamsAndSwitches(params1, acpSwitches2)
+    }).not.throws()
+  })
+
+  it('validateNonConflictParamsAndSwitches() when conflict exist', () => {
+    const params1 = {
+      a: '',
+      b: 'a',
+      c: 'b',
+    }
+
+    const acpSwitches2 = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateNonConflictParamsAndSwitches(params1, acpSwitches2)
+    }).throws()
+  })
+
+  it('validateAllRequiredSwitchesPresent() when required switches does not exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('a', 'long')
+
+    const acpSwitches = {
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateAllRequiredSwitchesPresent(command, acpSwitches)
+    }).throws()
+  })
+
+  it('validateAllRequiredSwitchesPresent() when required switches exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('a', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateAllRequiredSwitchesPresent(command, acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateInputRequiredSwitchesHaveValue() when required switches does not have a value', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('e', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateInputRequiredSwitchesHaveValue(command, acpSwitches)
+    }).throws()
+  })
+
+  it('validateInputRequiredSwitchesHaveValue() when required switches are not present', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('z', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    // because 'z' is not present in the switches
+    expect(() => {
+      validateInputRequiredSwitchesHaveValue(command, acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateInputRequiredSwitchesHaveValue() when required switches are empty string', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('a', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    // because 'a' has a empty string value
+    expect(() => {
+      validateInputRequiredSwitchesHaveValue(command, acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateInputRequiredSwitchesHaveValue() when required switches are boolean', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('e', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    // because 'e' doesn't a value
+    expect(() => {
+      validateInputRequiredSwitchesHaveValue(command, acpSwitches)
+    }).throws()
+  })
+
+  it('validateInputRequiredSwitchesHaveValue() when required switches have a value', () => {
+    const command = new Command('/cmd1/:param1')
+    command.requiredSwitch('f', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    // because 'f' has value it doens't throw error
+    expect(() => {
+      validateInputRequiredSwitchesHaveValue(command, acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateInputValuedSwitchesWithNoDefaultHaveValue() when value exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('f', 'long', null)
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateInputValuedSwitchesWithNoDefaultHaveValue(command, acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateInputValuedSwitchesWithNoDefaultHaveValue() when value not exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('e', 'long', null)
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateInputValuedSwitchesWithNoDefaultHaveValue(command, acpSwitches)
+    }).throws()
+  })
+
+  it('validateInputBooleanSwitchesDontHaveValues() when value exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.booleanSwitch('e', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateInputBooleanSwitchesDontHaveValues(command, acpSwitches)
+    }).not.throws()
+  })
+
+  it('validateInputBooleanSwitchesDontHaveValues() when value not exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.booleanSwitch('f', 'long')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    expect(() => {
+      validateInputBooleanSwitchesDontHaveValues(command, acpSwitches)
+    }).throws()
+  })
+
+  it('defaultShortValuedSwitchesThatNeedsToBeAdded() value exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('f', 'long', 'defaultValue')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    const switches = defaultShortValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    expect(switches).eql({})
+  })
+
+  it('defaultShortValuedSwitchesThatNeedsToBeAdded() switch exist but doen not have value', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('e', 'long', 'defaultValue')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    const switches = defaultShortValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    // figuring out if valuedSwitches should have value is responsibility of other helper methods
+    expect(switches).eql({})
+  })
+
+  it('defaultShortValuedSwitchesThatNeedsToBeAdded() value not exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('c', 'long', 'defaultValue')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+    }
+
+    const switches = defaultShortValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    expect(switches).eql({ c: 'defaultValue' })
+  })
+
+  it('defaultShortValuedSwitchesThatNeedsToBeAdded() value exit but for long switch', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('c', 'long', 'defaultValue')
+
+    const acpSwitches = {
+      a: '',
+      e: true,
+      f: 'f',
+      long: 'val',
+    }
+
+    const switches = defaultShortValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    expect(switches).eql({})
+  })
+
+  //------------
+
+  it('defaultLongValuedSwitchesThatNeedsToBeAdded() value exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('f', 'long', 'defaultValue')
+
+    const acpSwitches = {
+      long: 'x',
+      longer: true,
+    }
+
+    const switches = defaultLongValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    expect(switches).eql({})
+  })
+
+  it('defaultLongValuedSwitchesThatNeedsToBeAdded() switch exist but doen not have value', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('e', 'longer', 'defaultValue')
+
+    const acpSwitches = {
+      long: 'x',
+      longer: true,
+    }
+
+    const switches = defaultLongValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    // figuring out if valuedSwitches should have value is responsibility of other helper methods
+    expect(switches).eql({})
+  })
+
+  it('defaultLongValuedSwitchesThatNeedsToBeAdded() value not exist', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('c', 'longest', 'defaultValue')
+
+    const acpSwitches = {
+      long: 'x',
+      longer: true,
+    }
+
+    const switches = defaultLongValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    expect(switches).eql({ longest: 'defaultValue' })
+  })
+
+  it('defaultLongValuedSwitchesThatNeedsToBeAdded() value exit but for short switch', () => {
+    const command = new Command('/cmd1/:param1')
+    command.valuedSwitch('c', 'longest', 'defaultValue')
+
+    const acpSwitches = {
+      long: 'x',
+      longer: true,
+      c: 'val',
+    }
+
+    const switches = defaultLongValuedSwitchesThatNeedsToBeAdded(
+      command,
+      acpSwitches
+    )
+
+    expect(switches).eql({})
+  })
+
+  it('cleanSwitchValues()', () => {
+    const acpSwitches = {
+      a: [],
+      b: ['cmd1'],
+      c: ['cmd2'],
+    }
+
+    expect(cleanSwitchValues(acpSwitches)).eql({
+      a: true,
+      b: 'cmd1',
+      c: 'cmd2',
+    })
+  })
+
+  it('mapCommandParamNamesAndValues() when inputs have differnt length', () => {
+    const commandParamNames = ['param1']
+    const commandParamValues = ['val1', 'val2']
+
+    expect(() => {
+      mapCommandParamNamesAndValues(commandParamNames, commandParamValues)
+    }).throws()
+  })
+
+  it('mapCommandParamNamesAndValues()', () => {
+    const commandParamNames = ['param1', 'param2']
+    const commandParamValues = ['val1', 'val2']
+
+    expect(
+      mapCommandParamNamesAndValues(commandParamNames, commandParamValues)
+    ).eql({ param1: 'val1', param2: 'val2' })
+  })
+})
