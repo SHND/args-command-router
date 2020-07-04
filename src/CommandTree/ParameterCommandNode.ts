0820301c73fdf19a519e1d91ad024ad30c3d179d
Sahand Javid <sahand.javid@gmail.com>
2020-07-03 18:33:32 -0700

    Remove old files

--- src/CommandTree/ParameterCommandNode.ts
+++
@@ -1,8 +0,0 @@
-import CommandNode from './CommandNode'
-import { PARAMETER_PREFIX } from '../constants'
-
-export default class ParameterCommandNode extends CommandNode {
-  constructor() {
-    super(PARAMETER_PREFIX)
-  }
-}
