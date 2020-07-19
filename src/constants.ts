c8f36ab5e16f6d91cf0b12a113663ed395c2b609
Sahand Javid <sahand.javid@gmail.com>
2020-07-19 11:27:24 -0700

    Some comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,19 @@
+/**
+ * Symbol for separating pathItems in a route string.
+ */
 export const PATH_ITEM_DELIMITER = '/';
 
+/**
+ * Prefix which determines that the PathItem is a dynamic path item.
+ */
 export const DYNAMIC_PATH_PREFIX = ':';
 
+/**
+ * Open symbol for switchRouter pathItem expressions.
+ */
 export const OPEN_SWITCH_EXPR_SYMBOL = '[';
 
+/**
+ * Close symbol for switchRouter pathItem expressions.
+ */
 export const CLOSE_SWITCH_EXPR_SYMBOL = ']';
